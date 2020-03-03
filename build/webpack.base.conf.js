bc02ec276f2a447685184195b1aceff62e73b685
Alexander Fomin <firstmixlucker@gmail.com>
2020-03-03 18:14:50 +0300

    add file

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,121 @@
+const path = require('path')
+const fs = require("fs");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/',
+}
+
+const PAGES_DIR = `${PATHS.src}/`;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter(fileName => fileName.endsWith(".pug"));
+
+module.exports = {
+
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[contenthash].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        // Pug
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      }, {
+        // JavaScript
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }, {
+        // Fonts
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }, {
+        // images / icons
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }, {
+        // scss
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: { path: './postcss.config.js' }
+            }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ],
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: { path: './postcss.config.js' }
+            }
+          }
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[contenthash].css`
+    }),
+    ...PAGES.map(
+      page =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, '.html')}`
+        })
+    )
+
+  ],
+}
---
+++ build/webpack.build.conf.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const buildWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin()
+  ]
+});
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(buildWebpackConfig)
+})
