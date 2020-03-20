5a5231070aeffff7429cd70f928145caac993f78
Alexander Fomin <firstmixlucker@gmail.com>
2020-03-20 19:21:30 +0300

    изменил скрип формирования календарных чисел и добавил event для стрелок

--- src/components/form-elements/form-date/script.js
+++ src/components/form-elements/form-date/script.js
@@ -1,21 +1,22 @@
 {
-  const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-  const date = new Date();
-  // date.setFullYear(2020);
-  date.setMonth(7);
-  const nowYear = date.getFullYear();
-  const nowMonth = date.getMonth();
-  const nowWeekday = date.getDay();
-  const nowDay = date.getDate();
-
   /* Functions */
-  const getDay = (day = nowDay, month = nowMonth, year = nowYear) =>
-    new Date(year, month, day).getDate();
+  const setDaysOnDate = (month = 0, year = 1970) => {
+    /* Variables */
+    const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    const date = new Date();
+    // Set date
+    date.setFullYear(year, month);
+    const nowYear = date.getFullYear();
+    const nowMonth = date.getMonth();
+    // const nowWeekday = date.getDay();
+    const nowDay = date.getDate();
+    /* Functions */
+    const getDay = (day = nowDay, month = nowMonth, year = nowYear) =>
+      new Date(year, month, day).getDate();
 
-  const getWeakday = (day = nowDay, month = nowMonth, year = nowYear) =>
-    new Date(year, month, day).getDay();
-
-  const setDaysOnDate = () => {
+    const getWeakday = (day = nowDay, month = nowMonth, year = nowYear) =>
+      new Date(year, month, day).getDay();
+    /* Usage */
     // Set name of month
     const nameMonth = document.querySelector('.form-date .header__text');
     nameMonth.textContent = months[nowMonth] + ' ' + nowYear;
@@ -38,15 +39,56 @@
         item.textContent = k++;
       }
     }
-    // Delete last weak
+    // delete last weak
+    // const totalElementsOfCalendar = document.querySelectorAll('.form-date .content__day').length; (? && totalElementsOfCalendar === 42)
     if (firstWeakdayOfMonth + lastDayOfMonth < 36) {
       var parent = document.querySelector('.form-date .content');
       for (let i = 0; i < 7; i++) {
         parent.removeChild(parent.lastElementChild);
       }
     }
+    // Set style .actualDay
+    const actualDate = new Date();
+    if (date.getMonth() === actualDate.getMonth() && date.getFullYear() === actualDate.getFullYear()) {
+      const days = document.querySelectorAll('.form-date .content__day');
+      days[firstWeakdayOfMonth + actualDate.getDate() - 1].classList.add('actualDay');
+    }
+  }
+  const changeMonthOfCalendar = () => {
+
+  }
+  /* Function Events */
+  const clickOnArrow = e => {
+    const element = e.target;
+    let stringClass = element.classList.value;
+    // console.dir(stringClass.includes('right'));
+    // console.dir(element);
+    if (stringClass.includes('box')) {
+      stringClass = element.children[0].classList.value
+    }
+
+    if (stringClass.includes('right')) {
+      console.log('right')
+      // month++;
+      // setDaysOnDate(month, nowYear)
+    } else if (stringClass.includes('left')) {
+      console.log('left')
+      // month--;
+      // setDaysOnDate(month, nowYear)
+    }
   }
 
   /* Usage */
-  setDaysOnDate();
+  const date = new Date();
+  const nowYear = date.getFullYear();
+  const nowMonth = date.getMonth();
+  const nowDay = date.getDate();
+  let month = nowMonth;
+  let year = nowYear;
+  setDaysOnDate(nowMonth, nowYear); // default: month = 0, year = 1970
+
+  document.querySelectorAll('.header__arrow-box').forEach(e => {
+    e.addEventListener('click', clickOnArrow);
+  });
+
 }
